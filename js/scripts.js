9e9902cf0323a14bac3399899a90f828009af8c0
kalisam <37503662+kalisam@users.noreply.github.com>
2021-03-25 14:34:39 -0400

    updates
    added holotoken
    added new api call to get coin history
    misc random changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,84 @@
 
 $(document).ready(function () {
-    // Define currency codes
-    const bitcoin = "Qwsogvtv82FCd"
-    const etherium = "razxDUgYGNAdQ"
-    const litecoin = "D7B1x_ks7WhV5"
-    var uuid = "Qwsogvtv82FCd";
-    // Define timeframe codes
+    const bitcoin = "Qwsogvtv82FCd";
+    const ethereum = "razxDUgYGNAdQ";
+    const litecoin = "D7B1x_ks7WhV5";
+	const holotoken = "iEHCPwcxoIH8e";
+    let uuid = "Qwsogvtv82FCd";
     const yearly = "1y";
     const monthly = "30d";
     const weekly = "7d";
     const daily = "24h";
-    var time = "24h";
-    var currencyName = "Bitcoin"
-    // Define empty chart globally, then call getCoinData to populate default chart (btc)
-    let myChart;
-    getCoinData(uuid, time);
-
-    // Timeframe selector
+	const coinRankingApiKey = "INSERT COINRANKING API KEY HERE";
+    let time = "24h";
+    let currencyName = "Bitcoin";
+    //getCoinData(uuid, time);
+	
     $('input:radio[name=options]').on("click", function () {
         if (time != $("input[name=options]:checked").val()) {
-            time = $("input[name=options]:checked").val();
+				time = $("input[name=options]:checked").val();
             console.log(time);
-            destroyerOfCharts();
-            getCoinData(uuid, time);
         }
-    })
-
-    // Currency type selector
+	})
+		
+	/* $('input:radio[name=dataOptions]').on("click", function () {
+		if (functionName != $("input[name=dataOptions]:checked").val()) {
+				functionName = $("input[name=dataOptions]:checked").val();
+			console.log(functionName);
+		}
+	}) */
+	
     $('#cryptoList').change(function () {
         var selectedValueCurrency = parseInt($(this).val());
-        destroyerOfCharts(uuid);
-        //Depends on Value 0-2 respective function gets called. 
+        
         switch (selectedValueCurrency) {
             case 0:
-                console.log("radio btc success");
+                console.log("radio btc call");
                 $("#currency").text("Bitcoin");
                 uuid = bitcoin;
-                getCoinData(uuid, time);
                 break;
             case 1:
-                console.log("radio eth success");
-                $("#currency").text("Etherium");
-                uuid = etherium;
-                getCoinData(uuid, time);
+                console.log("radio eth call");
+                $("#currency").text("Ethereum");
+                uuid = ethereum;
                 break;
             case 2:
-                console.log("radio ltc success");
+                console.log("radio ltc call");
                 $("#currency").text("Litecoin");
                 uuid = litecoin;
-                getCoinData(uuid, time);
+                break;
+			case 3:
+				console.log("radio hot call");
+				$("#currency").text("Holotoken");
+                uuid = holotoken;
+                break;
         }
     });
 
+	$('#getData').on("click" , function() {
+		console.log("getData btn clicked");
+		console.log($('input:radio[name=data-Options]:checked').val());
+		//if dataOptions radio value is history getCoinHistory(uuid, time)
+		if ($('input:radio[name=data-Options]:checked').val() == "History") {
+			getCoinHistory(uuid, time);
+		} else {
+		//else if dataOptions radio value is info getCoinData(uuid,time)
+			if ($('input:radio[name=data-Options]:checked').val() == "Info") {
+				getCoinData(uuid, time);
+			}
+		}
+	})
+	
     function getCoinData(currency, timeframe) {
-        console.log("getCoinData Success");
+        console.log("getCoinData called");
 
         var baseUrl = "https://api.coinranking.com/v2/coin/" + currency + "?timePeriod=" + timeframe;
-        var proxyUrl = "https://cors-anywhere.herokuapp.com/";
-        var apiKey = "coinrankingc0b595008db85657a50d4082f20ff1ab68d03f2b78445fb8"
-        fetch(`${proxyUrl}${baseUrl}`, {
+        var proxyUrl = "https://cors.bridged.cc/";
+        fetch(proxyUrl + baseUrl, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'X-My-Custom-Header': `${apiKey}`,
+                'X-My-Custom-Header': coinRankingApiKey,
                 'Access-Control-Allow-Origin': "*"
             }
         })
@@ -74,24 +90,55 @@ $(document).ready(function () {
                     })
                 }
             })
+		console.log("getCoinData Success");
+    }
+	
+	function getCoinHistory(currency, timeframe) {
+        console.log("getCoinHistory Called");
+
+        var baseUrl = "https://api.coinranking.com/v2/coin/" + currency + "/history/?timePeriod=" + timeframe;
+        var proxyUrl = "https://cors.bridged.cc/";
+        fetch(proxyUrl + baseUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-My-Custom-Header': coinRankingApiKey,
+                'Access-Control-Allow-Origin': "*"
+            }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    response.json().then((json) => {
+                        console.log("getCoinHistoryResponse Success");
+                        handlerFunctionB(json.data);
+                    })
+                }
+            })
+		console.log("getCoinHistory Success");
+
     }
     function handlerFunctionA(data) {
         console.log(data);
         let coinsData = data.coin;
+		//update text on page
+		$("#coin-img").attr("src" , coinsData.iconUrl);
+		$("#current-price").text('$' + coinsData.price);
         (function () {
-            // Append current price, Name, and Img
-            var price = Math.round((parseFloat(coinsData.price) + Number.EPSILON) * 100) / 100;
-            $("#currentPrice").text(price);
-            $("img").attr("src", coinsData.iconUrl);
+            
             // Graphs
             var ctx = document.getElementById('myChart')
-            myChart = new Chart(ctx, {
+            // eslint-disable-next-line no-unused-vars
+            if(window.myCharts !== undefined){
+				window.myCharts.destroy();
+				console.log("destroying old chart")
+			}
+			window.myCharts = new Chart(ctx, {
                 type: 'line',
                 data: {
-                    labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+                    labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
                     datasets: [{
                         data: coinsData.sparkline,
-                        label: coinsData.symbol,
+                        label: coinsData.name,
                         lineTension: 0,
                         backgroundColor: 'transparent',
                         borderColor: coinsData.color,
@@ -104,28 +151,71 @@ $(document).ready(function () {
                     scales: {
                         yAxes: [{
                             ticks: {
-                                beginAtZero: false,
-                                callback: function (value, index, values) {
-                                    return '$' + value;
-                                }
+                                beginAtZero: false
                             }
                         }]
                     },
                     legend: {
-                        display: true,
+                        display: false
                     }
                 }
             })
         })()
     }
-    function destroyerOfCharts() {
-        if (myChart) {
-            console.log("destroying old chart.");
-            myChart.destroy();
-            $("#currentPrice").text("");
-            $("img").attr("src", "");
-        }
+	function handlerFunctionB(data) {
+        console.log(data);
+        let coinsData = data;
+		var prices = [coinsData.history.length];
+		var timestamps = [coinsData.history.length];
+		var x;
+		// Don't update text on page
+		//$("#coin-img").attr("src" , coinsData.iconUrl);
+		//$("#current-price").text('$' + coinsData.price);
+		//for each history price push timestamp converted from milliseconds to an array and push price at corresponding entry
+		for (x in coinsData.history) {
+			prices[x] = coinsData.history[x].price;
+			timestamps[x] = coinsData.history[x].timestamp;
+		}		
+		console.log(prices);
+		console.log(timestamps);
+		 
+         (function () {
+            
+            // Graphs
+            var ctx = document.getElementById('myChart')
+            // eslint-disable-next-line no-unused-vars
+            if(window.myCharts !== undefined){
+				window.myCharts.destroy();
+				console.log("destroying old chart")
+			}
+			window.myCharts = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: timestamps,
+                    datasets: [{
+                        data: prices,
+                        label: coinsData.name,
+                        lineTension: 0,
+                        backgroundColor: 'transparent',
+                        borderColor: coinsData.color,
+                        borderWidth: 4,
+                        pointBackgroundColor: coinsData.color,
+                    },
+                    ]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: false
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            }) 
+        } )()
     }
 })
-
-// })
