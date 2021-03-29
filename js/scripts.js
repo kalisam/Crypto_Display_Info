d4822c69f4aca39b055605d425962dbdfc11ff34
kalisam <37503662+kalisam@users.noreply.github.com>
2021-03-29 14:34:19 -0400

    Updated lots of things
    changed cryptolist populate itself based on coins object; easy to add new coins now
    fixed coinhistory date
    misc html text updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,87 +1,78 @@
-
 $(document).ready(function () {
-    const bitcoin = "Qwsogvtv82FCd";
-    const ethereum = "razxDUgYGNAdQ";
-    const litecoin = "D7B1x_ks7WhV5";
-	const holotoken = "iEHCPwcxoIH8e";
+    //object holding coins and corresponding uuid
+    const coins = {
+        "Bitcoin": "Qwsogvtv82FCd",
+        "Ethereum": "razxDUgYGNAdQ",
+        "Litecoin": "D7B1x_ks7WhV5",
+        "Holotoken": "iEHCPwcxoIH8e"
+    };
     let uuid = "Qwsogvtv82FCd";
     const yearly = "1y";
     const monthly = "30d";
     const weekly = "7d";
     const daily = "24h";
-	const coinRankingApiKey = "INSERT COINRANKING API KEY HERE";
+    const coinRankingApiKey = "INSERT API KEY";
     let time = "24h";
-    let currencyName = "Bitcoin";
-    //getCoinData(uuid, time);
-	
-    $('input:radio[name=options]').on("click", function () {
-        if (time != $("input[name=options]:checked").val()) {
-				time = $("input[name=options]:checked").val();
+
+
+    let numLength = function (n) {
+        //console.log("" + n + ":" + String(n).length);
+        //convert to string and get string length; COUNTS . in numlength
+        return (("" + n).length);
+    }
+
+    $('input[name=timeFrameOption]').on("click", function () {
+        if (time != $("input[name=timeFrameOption]:checked").val()) {
+            time = $("input[name=timeFrameOption]:checked").val();
             console.log(time);
         }
-	})
-		
-	/* $('input:radio[name=dataOptions]').on("click", function () {
-		if (functionName != $("input[name=dataOptions]:checked").val()) {
-				functionName = $("input[name=dataOptions]:checked").val();
-			console.log(functionName);
-		}
-	}) */
-	
+    })
+
+    //populate crypto dropdown selection list with all coins in coins           
+    for (item in coins) {
+        //console.log(item);
+        //console.log(`<option value="${item}"> ${item} </option>`);
+        $('#cryptoList').append(`<option value="${item}"> ${item} </option>`);
+    }
+
+    //whenever cryptolist changes update selected currency text and uuid
     $('#cryptoList').change(function () {
-        var selectedValueCurrency = parseInt($(this).val());
-        
-        switch (selectedValueCurrency) {
-            case 0:
-                console.log("radio btc call");
-                $("#currency").text("Bitcoin");
-                uuid = bitcoin;
-                break;
-            case 1:
-                console.log("radio eth call");
-                $("#currency").text("Ethereum");
-                uuid = ethereum;
-                break;
-            case 2:
-                console.log("radio ltc call");
-                $("#currency").text("Litecoin");
-                uuid = litecoin;
-                break;
-			case 3:
-				console.log("radio hot call");
-				$("#currency").text("Holotoken");
-                uuid = holotoken;
-                break;
-        }
+        var selectedCurrency = $(this).val();
+        $("#currency").text(selectedCurrency);
+        console.log(coins[selectedCurrency]);
+        uuid = coins[selectedCurrency];
+        console.log("uuid is set to " + uuid);
     });
 
-	$('#getData').on("click" , function() {
-		console.log("getData btn clicked");
-		console.log($('input:radio[name=data-Options]:checked').val());
-		//if dataOptions radio value is history getCoinHistory(uuid, time)
-		if ($('input:radio[name=data-Options]:checked').val() == "History") {
-			getCoinHistory(uuid, time);
-		} else {
-		//else if dataOptions radio value is info getCoinData(uuid,time)
-			if ($('input:radio[name=data-Options]:checked').val() == "Info") {
-				getCoinData(uuid, time);
-			}
-		}
-	})
-	
+    //get data button is clicked, either get coin info or get coin history
+    //TODO just call both at once with a single graph function
+    $('#getData').on("click", function () {
+        console.log("getData btn clicked");
+        console.log($('input:radio[name=data-Options]:checked').val());
+        //if dataOptions radio value is history getCoinHistory(uuid, time)
+        if ($('input:radio[name=data-Options]:checked').val() == "History") {
+            getCoinHistory(uuid, time);
+        } else {
+            //else if dataOptions radio value is info getCoinData(uuid,time)
+            if ($('input:radio[name=data-Options]:checked').val() == "Info") {
+                getCoinData(uuid, time);
+            }
+        }
+    })
+    //TODO integrate these four functions better; maybe just one function api caller; but howto format graph data depending on which fn
     function getCoinData(currency, timeframe) {
         console.log("getCoinData called");
 
         var baseUrl = "https://api.coinranking.com/v2/coin/" + currency + "?timePeriod=" + timeframe;
         var proxyUrl = "https://cors.bridged.cc/";
         fetch(proxyUrl + baseUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-My-Custom-Header': coinRankingApiKey,
-                'Access-Control-Allow-Origin': "*"
-            }
-        })
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': coinRankingApiKey,
+                    'Access-Control-Allow-Origin': "*"
+                }
+            })
             .then((response) => {
                 if (response.ok) {
                     response.json().then((json) => {
@@ -90,22 +81,22 @@ $(document).ready(function () {
                     })
                 }
             })
-		console.log("getCoinData Success");
+        console.log("getCoinData Success");
     }
-	
-	function getCoinHistory(currency, timeframe) {
+
+    function getCoinHistory(currency, timeframe) {
         console.log("getCoinHistory Called");
 
         var baseUrl = "https://api.coinranking.com/v2/coin/" + currency + "/history/?timePeriod=" + timeframe;
         var proxyUrl = "https://cors.bridged.cc/";
         fetch(proxyUrl + baseUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-My-Custom-Header': coinRankingApiKey,
-                'Access-Control-Allow-Origin': "*"
-            }
-        })
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': coinRankingApiKey,
+                    'Access-Control-Allow-Origin': "*"
+                }
+            })
             .then((response) => {
                 if (response.ok) {
                     response.json().then((json) => {
@@ -114,25 +105,30 @@ $(document).ready(function () {
                     })
                 }
             })
-		console.log("getCoinHistory Success");
+        console.log("getCoinHistory Success");
 
     }
+
     function handlerFunctionA(data) {
         console.log(data);
         let coinsData = data.coin;
-		//update text on page
-		$("#coin-img").attr("src" , coinsData.iconUrl);
-		$("#current-price").text('$' + coinsData.price);
+        //update img and text on page
+        $("#coin-img").attr("src", coinsData.iconUrl);
+        $("#current-price").text('$' + coinsData.price);
+
+        //TODO Make drawgraph a single function
         (function () {
-            
+
             // Graphs
             var ctx = document.getElementById('myChart')
-            // eslint-disable-next-line no-unused-vars
-            if(window.myCharts !== undefined){
-				window.myCharts.destroy();
-				console.log("destroying old chart")
-			}
-			window.myCharts = new Chart(ctx, {
+            
+            //if there is an old chart delete it
+
+            if (window.myCharts !== undefined) {
+                window.myCharts.destroy();
+                console.log("destroying old chart")
+            }
+            window.myCharts = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
@@ -144,8 +140,7 @@ $(document).ready(function () {
                         borderColor: coinsData.color,
                         borderWidth: 4,
                         pointBackgroundColor: coinsData.color,
-                    },
-                    ]
+                    }, ]
                 },
                 options: {
                     scales: {
@@ -162,33 +157,42 @@ $(document).ready(function () {
             })
         })()
     }
-	function handlerFunctionB(data) {
+
+    function handlerFunctionB(data) {
         console.log(data);
         let coinsData = data;
-		var prices = [coinsData.history.length];
-		var timestamps = [coinsData.history.length];
-		var x;
-		// Don't update text on page
-		//$("#coin-img").attr("src" , coinsData.iconUrl);
-		//$("#current-price").text('$' + coinsData.price);
-		//for each history price push timestamp converted from milliseconds to an array and push price at corresponding entry
-		for (x in coinsData.history) {
-			prices[x] = coinsData.history[x].price;
-			timestamps[x] = coinsData.history[x].timestamp;
-		}		
-		console.log(prices);
-		console.log(timestamps);
-		 
-         (function () {
-            
+        var prices = [coinsData.history.length];
+        var timestamps = [coinsData.history.length];
+        var x;
+        // Don't update text on page
+        //$("#coin-img").attr("src" , coinsData.iconUrl);
+        //$("#current-price").text('$' + coinsData.price);
+
+        //for each history price:
+        // price at corresponding entry with length cut to a quarter of its length added to an array
+        // timestamp at each entry converted from milliseconds to array; getdatahistory returns date in SECONDS not ms, so tack on 000
+        
+        for (x in coinsData.history) {
+            prices[x] = (parseFloat(coinsData.history[x].price)).toFixed(numLength(coinsData.history[x].price) / 4);
+            //console.log(`${coinsData.history[x].timestamp}000`);
+            timestamps[x] = new Date(parseInt(`${coinsData.history[x].timestamp}000`));
+        }
+        console.log(prices);
+        console.log(timestamps);
+        
+        //TODO Make drawgraph a single function
+        (function () {
+
             // Graphs
             var ctx = document.getElementById('myChart')
             // eslint-disable-next-line no-unused-vars
-            if(window.myCharts !== undefined){
-				window.myCharts.destroy();
-				console.log("destroying old chart")
-			}
-			window.myCharts = new Chart(ctx, {
+
+            //if there is an old chart delete it
+            if (window.myCharts !== undefined) {
+                window.myCharts.destroy();
+                console.log("destroying old chart")
+            }
+            window.myCharts = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: timestamps,
@@ -200,8 +204,7 @@ $(document).ready(function () {
                         borderColor: coinsData.color,
                         borderWidth: 4,
                         pointBackgroundColor: coinsData.color,
-                    },
-                    ]
+                    }, ]
                 },
                 options: {
                     scales: {
@@ -215,7 +218,7 @@ $(document).ready(function () {
                         display: false
                     }
                 }
-            }) 
-        } )()
+            })
+        })()
     }
 })
